e23ae23041d777475a23f81c4b46c86bb41fd6ea
vijay-pandurangan <vijay.pandurangan@digital.ai>
2021-08-23 16:01:16 +0530

    First commit

---
+++ base.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+print("Loading the GOOGLE site")
+time.sleep(3)
+print("Waiting")
+driver.maximize_window()
+print("Maximizing the window")
+time.sleep(4)
+driver.quit()
