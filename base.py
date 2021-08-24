112b9ae44d57cf528cfe688624fbe188a3877061
vijay-pandurangan <vijay.pandurangan@digital.ai>
2021-08-24 18:18:14 +0530

    [ENG-6859] Q2 2021 XLR _10.2.0_Sprint Hardening_Regression

--- base.py
+++ base.py
@@ -4,6 +4,7 @@ import time
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
 print("Loading the GOOGLE site")
+#time.sleep(6)
 time.sleep(3)
 print("Waiting")
 driver.maximize_window()
